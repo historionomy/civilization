aafc83064f1a9311dee1a971fb357ecf77f1ecbc
adescoqs <augustin.descoqs@gmail.com>
2024-06-07 00:49:13 +0200

    1st simulation

--- simulation.py
+++ simulation.py
@@ -12,6 +12,7 @@ import ast
 import numpy as np
 import scipy.interpolate
 import yaml
+from matplotlib import pyplot as plt
 
 # load parameters
 parameter_file = "parameters.yml"
@@ -19,8 +20,6 @@ parameter_file = "parameters.yml"
 parameters = {}
 with open(parameter_file, "r") as file:
     parameters = yaml.load(file, Loader=yaml.FullLoader)
-
-#max_number_of_cultures = 255
 
 class Simulation_data:
     def __init__(self):
@@ -39,14 +38,21 @@ class Simulation_data:
         # Constants
         self.natural_growth = None
 
+        # Variables
+        self.year = None
+
         # Functions
         self.fertility_per_technology_level = None
+
+    def set_max_population(self):
+        self.Pmax = self.ha_per_px*self.fertility_per_year[self.fertility_per_year['year']==self.year].iloc[0]["max_population_per_ha"]*self.fertility_map
 
     def reset_simulation(self):
         # Load image
         map_path = parameters['geographics']['map_path']  # Path to your map image
         self.map_img = cv2.imread(map_path)
         self.map_img = cv2.cvtColor(self.map_img, cv2.COLOR_BGR2RGB)
+        self.map_height, self.map_width, _ = self.map_img.shape
 
         # Compute geographical dependant coefficient for fertility and population diffusivity
         geographics_params = [ {**geo_param, "RGB" : ast.literal_eval(geo_param['RGB']) } for geo_param in  parameters['geographics']['zones']]
@@ -72,6 +78,71 @@ class Simulation_data:
 
         # Load constants
         self.natural_growth = parameters['demographics']['natural_growth']
+        self.ha_per_px = parameters['geographics']['ha_per_px']
+        self.start_year = parameters['time']['start_year']
+        self.end_year = parameters['time']['end_year']
+        self.time_step = parameters['time']['time_step']
 
         # Functions
-        self.fertility_per_technology_level = pd.DataFrame(parameters['fertility_per_technology_level'])       
+        self.fertility_per_technology_level = pd.DataFrame(parameters['fertility_per_technology_level'])
+        years = sorted(list(dict.fromkeys(self.fertility_per_technology_level['year'].tolist()+list(range(self.start_year, self.end_year + 1 ,1)))))
+        self.fertility_per_year=pd.merge(pd.DataFrame({ 'year' : years,}),self.fertility_per_technology_level[['year','max_population_per_ha']],on="year",how="left").interpolate(method='linear')     
+
+        #initialisation
+        self.year = self.start_year
+        self.population = np.zeros(self.map_img[:,:,0].shape)
+        ## TODO parametrize population start
+        self.population[800,800] = 1
+        self.set_max_population()
+
+    def view_field(self,field_name):
+        X = getattr(self, field_name)
+
+        match field_name:
+            case "population":
+                max_X = np.max(self.Pmax)
+            case _ :
+                max_X = np.max(X)
+
+        grayscale_array = np.array(255*X/max_X).astype('uint8')
+        overlay = np.zeros(self.map_img.shape)
+        overlay[:,:,0]= self.map_img[:,:,0]* (1-grayscale_array/255.0) + grayscale_array/255.0* grayscale_array
+        overlay[:,:,1]= self.map_img[:,:,1]* (1-grayscale_array/255.0) #+ grayscale_array/255.0* grayscale_array
+        overlay[:,:,2]= self.map_img[:,:,2]* (1-grayscale_array/255.0) #+ grayscale_array/255.0* grayscale_array
+
+        img = overlay.astype('uint8')
+        font                   = cv2.FONT_HERSHEY_SIMPLEX
+        bottomLeftCornerOfText = (10,40)
+        fontScale              = 1.5
+        fontColor              = (255,255,255)
+        thickness              = 5
+        lineType               = 1
+
+        time = self.year-self.start_year
+
+        img = cv2.putText(img,"Year="+f"{time:04}"+ ", Max "+field_name+"="+"{:.0f}".format(np.max(X)), 
+            bottomLeftCornerOfText, 
+            font, 
+            fontScale,
+            fontColor,
+            thickness,
+            lineType)
+
+        cv2.imwrite("img/"+field_name+"_"+f"{time:04}"+".png",cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+
+        return img
+    
+    def plot_field(self,field_name):
+        plt.imshow(self.view_field(field_name))
+        plt.show()
+
+    
+    def iterate(self):
+        self.set_max_population()
+        diffusion = scipy.ndimage.laplace(self.population)*self.population_diffusivity_map/100
+        diffusion[diffusion>10*self.natural_growth] = 10*self.natural_growth
+        diffusion[diffusion<-10*self.natural_growth] = -10*self.natural_growth
+        dP = self.natural_growth*self.population*(1-self.population/self.Pmax) + diffusion
+        self.population += dP*self.time_step
+        self.population[self.population < 0] = 1e-4
+        self.year += self.time_step
